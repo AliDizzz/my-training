56c38b40764ae834100e6b02cf20c142f3ea34a8
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-06-30 12:26:40 +0200

    route

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed in temporibus officiis. Quaerat, optio adipisci neque nisi harum explicabo eveniet corrupti maiores iure qui a. Itaque ab quo excepturi quisquam!</p>
+
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed in temporibus officiis. Quaerat, optio adipisci neque nisi harum explicabo eveniet corrupti maiores iure qui a. Itaque ab quo excepturi quisquam!</p>
+        </div>
+    );
+};
+
+export default About;
