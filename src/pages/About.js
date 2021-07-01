323f51ef76a634d7964bd14a93f7043b38d1724d
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-07-01 10:31:05 +0200

    logo

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
 
 const About = () => {
     return (
         <div>
             <Navigation />
+
+            <Logo />
             <h1>A propos</h1>
             <br />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed in temporibus officiis. Quaerat, optio adipisci neque nisi harum explicabo eveniet corrupti maiores iure qui a. Itaque ab quo excepturi quisquam!</p>
