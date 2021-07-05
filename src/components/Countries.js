c3d878593912379628f7356c7456e93b3d336533
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-07-05 10:21:37 +0200

    countries

---
+++ src/components/Countries.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Countries = () => {
+    const [data, setData] = useState([]);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Countries;
