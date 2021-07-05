706abcf4d6a9dd0021c8e526933404f05a7c8df1
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-07-05 10:35:40 +0200

    countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
+import axios from 'axios';
 
 const Countries = () => {
     const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag')
+            .then((res) => setData(res.data));
+    }, []);
+
     return (
         <div>
-            
+
         </div>
     );
 };
