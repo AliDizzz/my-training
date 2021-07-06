9efb82dc20fc330c6dc9642d8787cceb821ca7f0
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-07-06 12:43:01 +0200

    countries&cards

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -7,6 +7,7 @@ const Countries = () => {
     const [data, setData] = useState([]);
     const [sortedData, setSortedData] = useState([]);
     const [playOnce, setPlayOnce] = useState(true);
+    const [rangeValue, setRangeValue] = useState(40);
 
     useEffect(() => {
         if (playOnce) {
@@ -24,17 +25,29 @@ const Countries = () => {
             const sortedArray = countryObj.sort((a,b) => {
                 return b.population - a.population 
             });
+            sortedArray.length = rangeValue;
             setSortedData(sortedArray);
         };
         sortedCountry();
-    }, [data]);
+    }, [data, rangeValue, playOnce]);
 
     return (
         <div className="countries">
+            <div className="sort-container">
+                <input 
+                type="range" 
+                name="selectionner" 
+                min="1" 
+                max="250" 
+                value={rangeValue} 
+                onChange={
+                    (e) => setRangeValue(e.target.value)
+                }
+                />
+            </div>
             <ul className="countries-list">
                 {sortedData.map((country) => (
                     <Card country={country} key={country.name}/>
-                    
                 ))}
             </ul>
         </div>
